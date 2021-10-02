f29a71663bf037b330bf6e29e4d1fe420371d998
erhao bei <beierhao@hotmail.com>
2021-10-02 19:16:02 +0200

    update

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,8 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-12 text-center">
-          In developing
+        In developing
+        Image source: 
       </div>
     </div>
   </div>
