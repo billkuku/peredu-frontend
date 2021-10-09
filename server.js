24fabd9ec9ea9a970e8de828048887e216b24723
erhao bei <beierhao@hotmail.com>
2021-10-09 04:32:55 +0200

    update

--- server.js
+++ server.js
@@ -2,7 +2,18 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+const history = require('connect-history-api-fallback');
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(staticFileMiddleware);
+app.use(history({
+    disableDotRule: true,
+    verbose: true
+  }));
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/dist/index.html'));
+});
 const port = process.env.PORT || 3000;
 app.listen(port);
